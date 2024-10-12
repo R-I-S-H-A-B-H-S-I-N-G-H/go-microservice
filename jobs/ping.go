904853e321e35595eaff9a5bc1ca6eca6b8452ad
R-I-S-H-A-B-H-S-I-N-G-H <rishabhsingh2305@gmail.com>
2024-10-12 23:57:36 +0530

    added support for ping url

--- jobs/ping.go
+++ jobs/ping.go
@@ -1,14 +1,13 @@
 package jobs
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 )
 
 func PingMicroService() {
-	url := fmt.Sprintf("http://localhost:%s/ping", os.Getenv("PORT"))
+	url := os.Getenv("PING_URL)")
 
 	response, err := http.Get(url)
 	if err != nil {
